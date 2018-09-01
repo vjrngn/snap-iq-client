0274e7c517b3acec4174eb8e759a404fe24ab163
Suhas DS <suhas@totalcloud.io>
2018-09-01 17:45:07 +0530

    WIP login functionality

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import React, { Component } from "react";
 import Login from "./components/Login";
+import axios from "axios";
+import { Router, Route, Switch } from "react-router";
 
 class App extends Component {
+  state = {
+    email: "",
+    password: ""
+  };
+
+  onClick = () => {
+    axios
+      .post("http://192.168.1.111:3000/auth/login", {
+        email: this.state.email,
+        password: this.state.password
+      })
+      .then(function(response) {
+        localStorage.setItem("token", response.data.token);
+        if (localStorage.getItem("token") != null) {
+        }
+      });
+  };
+
   render() {
     return (
       <div
-        display="flex"
-        justify-content="center"
-        align-items="center"
-        height="100vh"
-        width="100vw"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          width: "100vw"
+        }}
       >
         <div>
-          <Login />
+          <Login
+            onEmailChange={value => this.setState({ email: value })}
+            onPasswordChange={value => this.setState({ password: value })}
+            onClick={this.onClick}
+          />
         </div>
       </div>
     );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,24 +1,50 @@
 import React from "react";
 import Input from "@material-ui/core/Input";
 import Button from "@material-ui/core/Button";
+import Divider from "@material-ui/core/Divider";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 
-function Login() {
+function Login(props) {
   return (
     <React.Fragment>
+      <AppBar position="static" color="default">
+        <Toolbar>
+          <Typography variant="title" color="primary">
+            Login
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Divider />
       <Input
         placeholder="Username"
+        disableUnderline={true}
         inputProps={{
           "aria-label": "Description"
         }}
+        onChange={e => props.onEmailChange(e.target.value)}
       />
-
+      <Divider />
       <Input
         placeholder="Password"
+        disableUnderline={true}
+        type="password"
         inputProps={{
           "aria-label": "Description"
         }}
+        onChange={e => props.onPasswordChange(e.target.value)}
       />
-      <Button color="primary">Login</Button>
+      <Divider />
+      <Button
+        variant="raised"
+        color="primary"
+        onClick={function() {
+          props.onClick();
+        }}
+      >
+        Login
+      </Button>
     </React.Fragment>
   );
 }
